044e2cd64d8ed93ba2de68ee30459513fb7b149f
sathvik-mn <sathvikmn2022@gmail.com>
2025-01-26 12:38:14 -0600

    notebook

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -25,6 +25,9 @@ class DataIngestion:
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
             
-            df.to_csv(self.ingestion_config. train)
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
+            logging.info("Train test split initiated")
+            train_test, test_split
+
         except :
             pass
