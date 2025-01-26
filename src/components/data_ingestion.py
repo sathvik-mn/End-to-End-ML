032e502225772dc179ccba7e73e958c6bcc4589b
sathvik-mn <sathvikmn2022@gmail.com>
2025-01-26 12:19:27 -0600

    notebook

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,3 +7,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+@dataclass
+class Datalngestionconfig:
+    train_data_path: str=os.path.join( 'artifacts' , "train.csv")
+    test_data_path: str=os.path.join( 'artifacts' , "test.csv")
+    raw_data_path: str=os.path.join( 'artifacts' , "data.csv")
